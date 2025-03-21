16e14e9cd55f8962695321e46201ca0e1b268bbc
shubham8080143 <shubhamthorat5041@gmail.com>
2025-03-21 21:19:39 +0530

    created project using web and lombok dependencies

---
+++ FileHandling/src/test/java/com/FileHandling/FileHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FileHandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
